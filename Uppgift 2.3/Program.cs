833dbaac5751f6241b1431fab32c2866bf0b2bf5
HerrBergenholtz <zbdv8800@gmail.com>
2023-09-15 12:59:50 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra denna fina bil?");
+            float dagar = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer planerar du att köra med bilen?");
+            float kilometer = float.Parse(Console.ReadLine());
+
+            float kostnad = (dagar * 500) + kilometer + 300 - 500;
+            Console.WriteLine("Det kommer att kosta dig totalt " +  kostnad + "kr att hyra denna bil.");
+
+            Console.ReadKey();
+        }
+    }
+}
